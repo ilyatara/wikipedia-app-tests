89672bbca3f86c9dd63a79510e0da3ef3ab62b1f
Ilya Tarasov <qailyatarasov@gmail.com>
2023-10-11 18:00:01 +0600

    Upload apk to browserstack automatically

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 import os
 from typing import Literal
 
+import requests
 from pydantic_settings import BaseSettings
 from pydantic import Field
 from typing import Optional
@@ -19,7 +20,7 @@ class Config(BaseSettings):
     device_name: str
     platform_version: str
 
-    app: str
+    apk_name: str
     app_wait_activity: str
 
     language: Optional[str] = None
@@ -30,12 +31,22 @@ class Config(BaseSettings):
     bstack_project_name: Optional[str] = None
     bstack_build_name: Optional[str] = None
     bstack_session_name: Optional[str] = None
+    bstack_app_url: Optional[str] = None
 
     timeout: float = Field(alias='selene_timeout', default=10.0)
 
     @staticmethod
     def get_context():
         return os.getenv('context')
+
+    def upload_apk_to_bstack(self):
+        bstack_upload_url = 'https://api-cloud.browserstack.com/app-automate/upload'
+        files = {'file': (self.apk_name,
+                          open(utils.path.get_path(self.apk_name), 'rb'),
+                          'multipart/form-data')}
+        auth = (self.bstack_username, self.bstack_access_key)
+        resp = requests.post(bstack_upload_url, auth=auth, files=files)
+        self.bstack_app_url = resp.json()['app_url']
 
     def to_driver_options(self):
         options = UiAutomator2Options()
@@ -48,7 +59,7 @@ class Config(BaseSettings):
         options.set_capability('locale', self.locale)
 
         if self.context == 'bstack':
-            options.set_capability('app', self.app)
+            options.set_capability('app', self.bstack_app_url)
             options.set_capability(
                 'bstack:options', {
                     'projectName': self.bstack_project_name,
@@ -59,9 +70,12 @@ class Config(BaseSettings):
                 },
             )
         else:
-            options.set_capability('app', utils.path.get_path(self.app))
+            options.set_capability('app', utils.path.get_path(self.apk_name))
 
         return options
 
 
 config = Config(_env_file=find_dotenv(f'.env.{Config.get_context()}'))
+
+if config.context == 'bstack' and config.bstack_app_url is None:
+    config.upload_apk_to_bstack()
