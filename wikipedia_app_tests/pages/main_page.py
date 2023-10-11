d587ef069af3a400ea53f06730b811d5b772629d
Ilya Tarasov <qailyatarasov@gmail.com>
2023-10-11 22:14:33 +0600

    Use helper function in MainPage

--- wikipedia_app_tests/pages/main_page.py
+++ wikipedia_app_tests/pages/main_page.py
@@ -1,11 +1,11 @@
 import allure
-from selene import browser, be
-from appium.webdriver.common.appiumby import AppiumBy
+from selene import be
+
+from wikipedia_app_tests.utils.appium import by_accessibility_id
 
 
 class MainPage:
-    search_input = browser.element(
-        (AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia"))
+    search_input = by_accessibility_id('Search Wikipedia')
 
     def should_be_opened(self):
         with allure.step('Check that the main screen is opened'):
