adefc1912da9b168577e882443029168e9fdfa1c
Ilya Tarasov <qailyatarasov@gmail.com>
2023-10-11 22:11:54 +0600

    Added tests for search

--- wikipedia_app_tests/utils/appium.py
+++ wikipedia_app_tests/utils/appium.py
@@ -2,5 +2,15 @@ from selene import browser
 from appium.webdriver.common.appiumby import AppiumBy
 
 
-def by_id(value):
+def by_id(value, all=False):
+    if all:
+        return browser.all((AppiumBy.ID, value))
     return browser.element((AppiumBy.ID, value))
+
+
+def by_accessibility_id(value):
+    return browser.element((AppiumBy.ACCESSIBILITY_ID, value))
+
+
+def by_xpath(value):
+    return browser.element((AppiumBy.XPATH, value))
